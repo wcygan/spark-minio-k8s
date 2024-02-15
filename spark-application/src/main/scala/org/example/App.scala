72ec2cdc0e71b481a3d9bc52c6946cf846085fce
Will Cygan <wcygan.io@gmail.com>
2024-02-15 00:35:52 -0600

    spark runs a simple application locally

--- spark-application/src/main/scala/org/example/App.scala
+++ spark-application/src/main/scala/org/example/App.scala
@@ -1,9 +1,32 @@
 package org.example
 
+import org.apache.spark.sql.SparkSession
+
 object App {
   def main(args: Array[String]): Unit = {
-    println(greeting())
-  }
+    // Initialize SparkSession
+    val spark = SparkSession.builder
+      .appName("OddNumbersCounter")
+      .master("local[*]")
+      .getOrCreate()
 
-  def greeting(): String = "Hello, world!"
+    import spark.implicits._
+
+    // Create a list of integers
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    // Convert the list to a DataFrame
+    val df = numbers.toDF()
+
+    // Filter the DataFrame to only contain odd numbers
+    val oddNumbers = df.filter($"value" % 2 === 1)
+
+    // Count the number of odd numbers
+    val count = oddNumbers.count()
+
+    println(s"The number of odd integers is $count")
+
+    // Stop the SparkSession
+    spark.stop()
+  }
 }
